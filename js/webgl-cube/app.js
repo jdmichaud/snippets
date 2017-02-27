f7b5ab17965f71c6e142e4b234f695815453373d
JD <jeandaniel.michaud@ge.com>
2017-02-27 17:12:27 +0100

    Move to 3D.

--- js/webgl-cube/app.js
+++ js/webgl-cube/app.js
@@ -1,19 +1,24 @@
 //
 // Following the youtube series starting at:
 // https://www.youtube.com/watch?v=kB0ZVUrI4Aw
+// glMatric documentation: http://glmatrix.net/
 //
 
 const vertexShaderSource = `
   precision mediump float;
 
-  attribute vec2 vertPosition;
+  attribute vec3 vertPosition;
 
   varying vec3 fragColor;
+
+  uniform mat4 mWorld;
+  uniform mat4 mView;
+  uniform mat4 mProj;
 
   void main()
   {
     fragColor = vec3(0.0, 0.0, 0.0);
-    gl_Position = vec4(vertPosition, 0.0, 1.0);
+    gl_Position = mProj * mView * mWorld * vec4(vertPosition, 1.0);
   }
 `;
 
@@ -27,6 +32,9 @@ const fragmentShaderSource = `
   }
 `;
 
+/*
+ * Create a shader, associate it with its GLSL code above and compile it.
+ */
 function initShader(gl, shaderSource, type) {
   const shader = gl.createShader(type);
   gl.shaderSource(shader, shaderSource);
@@ -37,6 +45,9 @@ function initShader(gl, shaderSource, type) {
   return shader;
 }
 
+/*
+ * Queue the shader into the program and link and valdate the program.
+ */
 function initProgram(gl, shaders) {
   const program = gl.createProgram();
   shaders.forEach((shader) => gl.attachShader(program, shader));
@@ -52,6 +63,9 @@ function initProgram(gl, shaders) {
   return program;
 }
 
+/*
+ * Create the shader and the program.
+ */
 function initGL(gl) {
   gl.clearColor(1.0, 1.0, 1.0, 1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
@@ -63,6 +77,15 @@ function initGL(gl) {
   return program;
 }
 
+function setUniform(gl, program, variable, matrix) {
+  const matUniformLocation = gl.getUniformLocation(program, variable);
+  gl.uniformMatrix4fv(matUniformLocation, gl.FALSE, matrix);
+  return matUniformLocation;
+}
+
+/*
+ * Insert an object described by a array of vertices into the rendering queue.
+ */
 function insertObject(gl, program, objectVertices) {
   var triangleVertexBufferObject = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexBufferObject);
@@ -70,13 +93,41 @@ function insertObject(gl, program, objectVertices) {
   const positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
   gl.vertexAttribPointer(
     positionAttribLocation,
-    2,
+    3, // Number of elements per attribute
     gl.FLOAT,
     gl.FALSE,
-    2 * Float32Array.BYTES_PER_ELEMENT,
-    0
+    3 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+    0 // Offset in the input array
   );
   gl.enableVertexAttribArray(positionAttribLocation);
+}
+
+function initTransform(gl, program) {
+  const worldMatrix = [
+    0.95, 0.0, 0.0, 0.0,
+    0.0, 0.95, 0.0, 0.0,
+    0.0, 0.0, 0.95, 0.0,
+    0.0, 0.0, 0.0, 1.0,
+  ];
+  const viewMatrix = new Float32Array(16);
+  mat4.lookAt(viewMatrix, [0, 0, -2], [0, 0, 0], [0, 1, 0]);
+  const projMatrix = new Float32Array(16);
+  mat4.perspective(projMatrix, Math.PI / 4, canvas.width / canvas.height, 0.1, 1000.0);
+
+  setUniform(gl, program, 'mWorld', new Float32Array(worldMatrix));
+  setUniform(gl, program, 'mView', viewMatrix);
+  setUniform(gl, program, 'mProj', new Float32Array(projMatrix));
+
+  return {
+    worldMatrix,
+    viewMatrix,
+    projMatrix,
+  };
+}
+
+function render() {
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  requestAnimationFrame(arguments.callee);
 }
 
 function main() {
@@ -87,14 +138,16 @@ function main() {
 
   // Create a triangle
   const objectVertices = [
-    0.0, 0.5,
-    -0.5, -0.5,
-    0.5, -0.5,
+   // X, Y, Z
+    0.0, 0.5, 0.0,
+    -0.5, -0.5, 0.0,
+    0.5, -0.5, 0.0,
   ];
   insertObject(gl, program, objectVertices);
 
   gl.useProgram(program);
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  transforms = initTransform(gl, program);
+  requestAnimationFrame(render);
 }
 
 main();
